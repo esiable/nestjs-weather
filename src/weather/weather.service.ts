be6b5c20b723e159bab1dbed530b3b9756d33ce2
Sebastian Kurcab <sebastian.kurcab@gmail.com>
2024-02-04 00:12:55 +0100

    cleaning some mistakes :)

--- src/weather/weather.service.ts
+++ src/weather/weather.service.ts
@@ -7,7 +7,7 @@ export class WeatherService {
   constructor(private readonly httpService: HttpService) {}
 
   async getWeather(city: string): Promise<any> {
-    const apiKey = '688237df04344461956194026240302';
+    const apiKey = '____';
     const apiUrl = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}`;
 
     return this.httpService
@@ -15,10 +15,4 @@ export class WeatherService {
       .pipe(map((response) => response.data))
       .toPromise();
   }
-
-  // getCurrentLocation(): string[] {
-  //   navigator.watchPosition((position) => {
-  //     return [position.coords.latitude, position.coords.longitude];
-  //   });
-  // }
 }
